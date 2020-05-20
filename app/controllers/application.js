43ea2c0ad45969110ef8302de95f2f3ef5dd990d
Barış Nişancı <bnisanci@e-kalite.com.tr>
2020-05-20 15:31:52 +0300

    Increase component counts

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,8 +4,8 @@ import _ from 'lodash'
 
 export default Controller.extend({
   appName: 'Ember Twiddle',
-  rowsCount: 100,
-  columnsCount: 15,
+  rowsCount: 300,
+  columnsCount: 20,
   shouldRenderItems: false,
   items: computed('rowsCount','columnsCount', function(){
     return _.map(_.range(0, this.get('rowsCount')) , (rowIndex) => {
