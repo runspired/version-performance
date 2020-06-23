76e74fcc05c816f1543c7173bce1d81c147b9b12
Chris Thoburn <runspired@gmail.com>
2020-06-23 14:19:41 -0700

    improve config

--- src/analyze.js
+++ src/analyze.js
@@ -69,16 +69,16 @@ async function normalizeConfig(config = {}) {
     }
 
     await add('use-yarn', true);
-    await add('control-sha', () => getShaForRef('origin/master'));
-    await add('experiment-sha', () => getShaForRef('HEAD'));
-    await add ('experiment-ref', () => getRefForHEAD())
     await add('build-control', true);
     await add('build-experiment', true);
+    await add('control-sha', () => config['build-control'] ? getShaForRef('origin/master') : '');
+    await add('experiment-sha', () => getShaForRef('HEAD'));
+    await add ('experiment-ref', () => getRefForHEAD())
     await add('control-dist', 'dist-control');
     await add('experiment-dist', 'dist-experiment');
     await add('control-build-command', `ember build -e production --output-path ${config['control-dist']}`);
     await add('experiment-build-command', `ember build -e production --output-path ${config['experiment-dist']}`);
-    await add('control-serve-command', `ember s --path=${config['control-dist']}`);
+    await add('control-serve-command', `ember s --path=${config['control-dist']} --port=4200`);
     await add('experiment-serve-command', `ember s --path=${config['experiment-dist']} --port=4201`);
     await add('control-url', 'http://localhost:4200?tracing=true');
     await add('experiment-url', 'http://localhost:4201?tracing=true');
